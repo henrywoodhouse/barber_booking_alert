bbc403375022e94afa290baf6071360d5f2d11b7
unknown <henry.woodhouse@hotmail.co.uk>
2020-06-01 19:30:11 -0400

    Fixing main.tf formatting error

--- main.tf
+++ main.tf
@@ -58,9 +58,31 @@ resource "aws_lambda_function" "barber_lambda" {
   source_code_hash = filebase64sha256("lambda/lambda_function_payload.zip")
 
   runtime = "python3.8"
+  timeout = 12
 
   environment {
     variables = {
-      AWS_REGION = ${var.aws_region}
+      AWS_REGION = var.aws_region
     }
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "run_hourly" {
+  name                = "every-one-hour"
+  description         = "Fires every hour"
+  schedule_expression = "rate(1 hour)"
+}
+
+resource "aws_cloudwatch_event_target" "run_barber_lambda_hourly" {
+  rule      = aws_cloudwatch_event_rule.run_hourly.name
+  target_id = "lambda"
+  arn       = aws_lambda_function.barber_lambda.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_barber_lambda" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.barber_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.run_hourly.arn
 }
